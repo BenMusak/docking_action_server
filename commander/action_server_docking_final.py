fbb95e7cf1709ccfc0ba6c386a17929da4320760
BenMusak <56428534+BenMusak@users.noreply.github.com>
2022-06-17 13:06:13 +0200

    Update action_server_docking_final.py
    Now it is fixed

--- commander/action_server_docking_final.py
+++ commander/action_server_docking_final.py
@@ -248,7 +248,7 @@ def controlDocking(minimal_publisher, rvecs, tvecs, dockingActionServer):
     pid_distanceZ = PID(distanceZ, 0.3, 0.1, 0.1)
     driveSpeedZ = pid_distanceZ
 
-    if angleDiff > targetDockingAng[2] and pid_distanceX > targetDockingPos[0] and pid_distanceZ > targetDockingPos[2] and completedDocking == False:
+    if angleDiff > targetDockingAng[2] or pid_distanceX > targetDockingPos[0] or pid_distanceZ > targetDockingPos[2] or completedDocking == False:
         minimal_publisher.angularVec = (0.0, 0.0, -turnSpeedZ)
         minimal_publisher.linearVec = (driveSpeedX, 0.0, driveSpeedZ)
         rclpy.spin_once(minimal_publisher)
@@ -256,7 +256,6 @@ def controlDocking(minimal_publisher, rvecs, tvecs, dockingActionServer):
         completedDocking = True
         print("Completed docking d8)")
         startFeeder(dockingActionServer)
-
 
 def startFeeder(dockingActionServer):
     dockingActionServer.dockSucced = True
